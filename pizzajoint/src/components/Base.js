4d1dbcd5835e2c6ddd175803c6a245c9afd38fce
Prince Nwaonicha <lolgfman@gmail.com>
2022-06-24 12:53:44 -0500

    added variants

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -2,14 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+
+const containerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  }
+}
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0
+  },
+  transition: {
+    type: 'spring',
+    stiffness: 120,
+    duration: 2
+  }
+}
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ opacity: 0, x: '100vw' }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+      variants={containerVariants}
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -28,12 +55,12 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
         >
           <Link to="/toppings">
             <motion.button
+              variants={nextVariants}
+              initial="hidden"
+              animate="visible"
             whileHover={{
             scale: 1.1,
             textShadow: "0px 0px 8motion.px rgb(255,255,255)",
