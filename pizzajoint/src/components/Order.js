93e4d744207bc6919bef178e5f281fcc7e89d4d5
Prince Nwaonicha <lolgfman@gmail.com>
2022-06-24 15:23:33 -0500

    finished putting variants

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import { motion } from 'framer-motion'
 
+const containerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4, //higher mass means it moves slower.
+      damping:8, // strength of opposing force.
+      when: "beforeChildren", // will complete transition before any children
+      staggerChildren: 0.4
+    }
+  }
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  }
+}
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
+      <motion.p
+      variants={childVariants}
+      >You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div
+      variants={childVariants}
+      >
+      
       {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
